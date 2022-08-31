3579e53cb856fa48cf8c3504da07e92dddb0d8ab
PrettyFlower <abigchode@gmail.com>
2022-08-31 16:50:20 -0400

    Move save game compatibility to the end of the function per Keriew.

--- src/building/building_state.c
+++ src/building/building_state.c
@@ -423,10 +423,6 @@ void building_state_load_from_buffer(buffer *buf, building *b, int building_buf_
     b->road_access_y = buffer_read_u8(buf);
     b->figure_id = buffer_read_i16(buf);
     b->figure_id2 = buffer_read_i16(buf);
-    if (save_version <= SAVE_GAME_CARAVANSERAI_LIGHTHOUSE_WRONG_ID && (b->type == BUILDING_LIGHTHOUSE || b->type == BUILDING_CARAVANSERAI)) {
-        b->figure_id = b->figure_id2;
-        b->figure_id2 = 0;
-    }
     b->immigrant_figure_id = buffer_read_i16(buf);
     b->figure_id4 = buffer_read_i16(buf);
     b->figure_spawn_delay = buffer_read_u8(buf);
@@ -536,6 +532,11 @@ void building_state_load_from_buffer(buffer *buf, building *b, int building_buf_
         b->sickness_doctor_cure = buffer_read_u8(buf);
         b->fumigation_frame = buffer_read_u8(buf);
         b->fumigation_direction = buffer_read_u8(buf);
+    }
+
+    if (save_version <= SAVE_GAME_CARAVANSERAI_LIGHTHOUSE_WRONG_ID && (b->type == BUILDING_LIGHTHOUSE || b->type == BUILDING_CARAVANSERAI)) {
+        b->figure_id = b->figure_id2;
+        b->figure_id2 = 0;
     }
 
     // The following code should only be executed if the savegame includes building information that is not 
