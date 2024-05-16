01b231197343505e358a623d8ef78cf712a549b0
Jose Cadete <crudelios@gmail.com>
2024-05-16 07:59:21 +0100

    Add support for user directories
    This commits separates the C3 data files from the user files, such as configurations, savegames, maps and custom scenario stuff.
    
    Upon first launch with this build, the game will ask whether the user wants to specify a directory and will also, in some cases recommend one.
    
    Base options are:
    - Default (the current way, everything is thrown into the Caesar 3 directory)
    - Default, with subdirectories - user files are still stored in Caesar 3, but in their own subdirectories
    - Recommended - a platform-specific recommendation. Depending on the system, there may not be a recommendation)
    - Custom - a user specified directory. depending on the system, this may not be available
    
    The game will also check if the provided directory is writeable and warn the user if it isn't.
    
    When changing the user directory, the user will be asked if he wants to copy their files to the new location and whether to overwrite files that already exist on the new location.
    
    Because this is a big change, there may be some bugs left. I tested most of the stuff, but I may have missed something.

--- src/platform/prefs.c
+++ src/platform/prefs.c
@@ -1,6 +1,7 @@
 #include "platform/prefs.h"
 
 #include "core/log.h"
+#include "core/file.h"
 #include "platform/platform.h"
 
 #include "SDL.h"
@@ -9,52 +10,82 @@
 #include <stdio.h>
 #include <string.h>
 
+static char stored_user_dir[FILE_NAME_MAX];
+static char stored_data_dir[FILE_NAME_MAX];
+
 static FILE *open_pref_file(const char *filename, const char *mode)
 {
-    #if SDL_VERSION_ATLEAST(2, 0, 1)
-    if (platform_sdl_version_at_least(2, 0, 1)) {
-        char *pref_dir = SDL_GetPrefPath("augustus", "augustus");
-        log_info("Location:", pref_dir, 0);
-        if (!pref_dir) {
-            return NULL;
-        }
-        size_t file_len = strlen(filename) + strlen(pref_dir) + 1;
-        char *pref_file = malloc(file_len * sizeof(char));
-        if (!pref_file) {
-            SDL_free(pref_dir);
-            return NULL;
-        }
-        snprintf(pref_file, file_len, "%s%s", pref_dir, filename);
-        SDL_free(pref_dir);
-
-        FILE *fp = fopen(pref_file, mode);
-        free(pref_file);
-        return fp;
+    char *pref_dir = platform_get_pref_path();
+    if (!pref_dir) {
+        return NULL;
     }
-    #endif
-    return NULL;
+    log_info("Location:", pref_dir, 0);
+    size_t file_len = strlen(filename) + strlen(pref_dir) + 1;
+    char *pref_file = malloc(file_len * sizeof(char));
+    if (!pref_file) {
+        SDL_free(pref_dir);
+        return NULL;
+    }
+    snprintf(pref_file, file_len, "%s%s", pref_dir, filename);
+    SDL_free(pref_dir);
+
+    FILE *fp = fopen(pref_file, mode);
+    free(pref_file);
+    return fp;
 }
 
 const char *pref_data_dir(void)
 {
-    static char data_dir[1000];
+    if (*stored_data_dir) {
+        return stored_data_dir;
+    }
+    char data_dir[FILE_NAME_MAX] = { 0 };
     FILE *fp = open_pref_file("data_dir.txt", "r");
     if (fp) {
-        size_t length = fread(data_dir, 1, 1000, fp);
+        size_t length = fread(data_dir, 1, FILE_NAME_MAX - 1, fp);
         fclose(fp);
         if (length > 0) {
             data_dir[length] = 0;
-            return data_dir;
         }
     }
-    return NULL;
+    snprintf(stored_data_dir, FILE_NAME_MAX, "%s", data_dir);
+    return stored_data_dir;
 }
 
 void pref_save_data_dir(const char *data_dir)
 {
+    snprintf(stored_data_dir, FILE_NAME_MAX, "%s", data_dir);
     FILE *fp = open_pref_file("data_dir.txt", "w");
     if (fp) {
         fwrite(data_dir, 1, strlen(data_dir), fp);
+        fclose(fp);
+    }
+}
+
+const char *pref_user_dir(void)
+{
+    if (*stored_user_dir) {
+        return stored_user_dir;
+    }
+    char user_dir[FILE_NAME_MAX] = { 0 };
+    FILE *fp = open_pref_file("user_dir.txt", "r");
+    if (fp) {
+        size_t length = fread(user_dir, 1, FILE_NAME_MAX - 1, fp);
+        fclose(fp);
+        if (length > 0) {
+            user_dir[length] = 0;
+        }
+    }
+    snprintf(stored_user_dir, FILE_NAME_MAX, "%s", user_dir);
+    return stored_user_dir;
+}
+
+void pref_save_user_dir(const char *user_dir)
+{
+    snprintf(stored_user_dir, FILE_NAME_MAX, "%s", user_dir);
+    FILE *fp = open_pref_file("user_dir.txt", "w");
+    if (fp) {
+        fwrite(user_dir, 1, strlen(user_dir), fp);
         fclose(fp);
     }
 }
