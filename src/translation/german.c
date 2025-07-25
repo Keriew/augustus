9d70427bccdbd0441c66568da77346b1d559d33e
Johannes-callidus <johannes@familie-puettmann.de>
2025-07-25 09:32:45 +0200

    Update german.c

--- src/translation/german.c
+++ src/translation/german.c
@@ -1695,7 +1695,8 @@ static translation_string all_strings[] = {
 	{TR_TOOLTIP_BUILDING_DISTRIBUTION_MAINTAINING, "Nur Karren-Depots können Waren entnehmen."},
 	{TR_CONFIG_STORAGE_STEP_4, "Nutze die Vielfachen von 4 zum Einteilen von Lagerplatz"},
 	{TR_CONFIG_MOVE_SAVINGS_TO_THE_RIGHT, "Ersparnisse neben den Wertungen anzeigen"},
-    {TR_CONFIG_NOT_ACCEPTING_MARKETS, "Märkte akzeptieren nichts, wenn sie gebaut werden" ,
+    {TR_CONFIG_NOT_ACCEPTING_MARKETS, "Märkte akzeptieren nichts, wenn sie gebaut werden"},
+    {TR_CONFIG_PATRICIAN_DEVOLUTION_FIX, "Häuser der Patrizier bilden biem Zusammenbruch keine 1x1 Häuser"},
 };
 
 void translation_german(const translation_string **strings, int *num_strings)
