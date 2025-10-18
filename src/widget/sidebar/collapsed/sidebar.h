edb0eaef94ce63a7484c73e3b456617dd4e55569
Rick Neeft <rick.neeft@outlook.com>
2025-10-19 00:26:09 +0200

    Removed the new_ and newsidebar folder. Move all types of sidebar into three distinct folders.

---
+++ src/widget/sidebar/collapsed/sidebar.h
@@ -0,0 +1,17 @@
+#ifndef WIDGET_COLLAPSED_SIDEBAR
+#define WIDGET_COLLAPSED_SIDEBAR
+
+#include "graphics/tooltip.h"
+#include "input/mouse.h"
+
+int collapsed_sidebar_width(void);
+
+void draw_collapsed_sidebar_background(int x_offset);
+void draw_collapsed_sidebar_foreground(void);
+
+int handle_collapsed_sidebar_mouse(const mouse *m);
+int handle_collapsed_sidebar_mouse_build_menu( const mouse *m);
+
+unsigned int get_collapsed_sidebar_tooltip_text(tooltip_context *c);
+
+#endif //  WIDGET_COLLAPSED_SIDEBAR
