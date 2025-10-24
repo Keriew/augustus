78596065ca9dff9848c64c681338859c8d976c83
Sephirex95 <15087297+Sephirex95@users.noreply.github.com>
2025-10-24 12:24:41 +0100

    fix endif

--- src/widget/map_editor.h
+++ src/widget/map_editor.h
@@ -20,10 +20,10 @@ void widget_map_editor_clear_current_tile(void);
 
 int widget_map_editor_get_grid_offset(void);
 
-#endif // WIDGET_MAP_EDITOR_H
-
 void widget_map_editor_clear_draw_context_event_tiles(void);
 
 int widget_map_editor_add_draw_context_event_tile(int grid_offset, int event_id);
 
 int widget_map_editor_get_event_id_at_grid_offset(int grid_offset);
+
+#endif // WIDGET_MAP_EDITOR_H
