8f1c7e38a77016a813e6210b70dad0e86ce8eedd
johannes <johannes@familie-puettmann.de>
2025-11-03 19:30:13 +0100

    add curly brakcets to all supported

--- src/widget/input_box.h
+++ src/widget/input_box.h
@@ -10,7 +10,7 @@
 #define INPUT_BOX_CHARS_ALPHANUMERIC  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "
 #define INPUT_BOX_CHARS_FILENAME      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-() "
 #define INPUT_BOX_CHARS_FORMULAS      "01234567890+-*/()[],{}" 
-#define INPUT_BOX_CHARS_ALL_SUPPORTED "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-()!\"\'*+,./:;=?[]_ "
+#define INPUT_BOX_CHARS_ALL_SUPPORTED "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-()!\"\'*+,./:;=?[]_{} "
 
 typedef struct {
     int x;
