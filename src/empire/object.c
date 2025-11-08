bdbbe6de625871650e10fe64a4fa1d2fdf15a2cb
johannes <johannes@familie-puettmann.de>
2025-11-08 10:32:33 +0100

    a

--- src/empire/object.c
+++ src/empire/object.c
@@ -673,7 +673,7 @@ empire_city_icon_type empire_object_get_random_icon_for_empire_object(full_empir
     } else {
         cityname = string_from_ascii("Invalid city name");
     } // fetch name
-    
+
     if (is_name_rome(cityname)) { // Rome
         array_size = sizeof(alloc_rome) / sizeof(empire_city_icon_type);
         random_array = alloc_rome;
