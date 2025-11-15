43b553fb1f5e23371b9e05973f1fd1f1672815b1
johannes <johannes@familie-puettmann.de>
2025-11-15 15:13:11 +0100

    oops

--- src/translation/english.c
+++ src/translation/english.c
@@ -1930,7 +1930,6 @@ static translation_string all_strings[] = {
     {TR_PARAMETER_TYPE_HOUSING_TYPE, "Housing level"},
     {TR_PARAMETER_BUILDING_MODEL_REST_CONFIRMATION, "This only resets the building model data."},
     {TR_CHEAT_DESTROYED_BUILDING, "Destroyed building"}
->>>>>>> b0deaa96553dc51284c23b328c3c24c1ed57d479
 };
 
 void translation_english(const translation_string **strings, int *num_strings)
