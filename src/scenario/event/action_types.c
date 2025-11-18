9c5fc1a5a5b5e59fc171be23e3351578fe140daa
johannes <johannes@familie-puettmann.de>
2025-11-18 21:59:39 +0100

    also clamp when executing

--- src/scenario/event/action_types.c
+++ src/scenario/event/action_types.c
@@ -18,6 +18,7 @@
 #include "city/trade.h"
 #include "city/victory.h"
 #include "core/random.h"
+#include "core/calc.h"
 #include "empire/city.h"
 #include "empire/object.h"
 #include "empire/trade_prices.h"
@@ -760,6 +761,8 @@ int scenario_action_type_change_model_data_execute(scenario_action_t *action)
     int data_type = action->parameter2;
     int amount = scenario_formula_evaluate_formula(action->parameter3);
     int set_to_value = action->parameter4;
+    
+    amount = calc_bound(amount, model_get_min_for_data_type(data_type), model_get_max_for_data_type(data_type));
 
     model_building *model_ptr = model_get_building(model);
 
@@ -840,6 +843,8 @@ int scenario_action_type_change_house_model_data_execute(scenario_action_t *acti
     int data_type = action->parameter2;
     int amount = scenario_formula_evaluate_formula(action->parameter3);
     int set_to_value = action->parameter4;
+    
+    amount = calc_bound(amount, model_get_min_for_house_data_type(data_type), model_get_max_for_house_data_type(data_type));
 
     model_house *model_ptr = model_get_house(model - 10); // convert from building type to housing
 
