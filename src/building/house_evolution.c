079c230c3da8c0594cab049fb22681d7263ab05b
miguelinileugim <Miguelinileugim@protonmail.com>
2025-11-19 15:49:54 +0100

    Decrease randomness further

--- src/building/house_evolution.c
+++ src/building/house_evolution.c
@@ -597,7 +597,7 @@ void building_house_process_evolve_and_consume_goods(void)
 
             // 1x1 houses only consume a quarter of the goods
             int random = random_byte();
-            if (b->house_size == 1 && game_time_day() % 16 && random % 2 // ie every 32 days average
+            if (b->house_size == 1 && game_time_day() % 8 && random % 4 // ie every 32 days average
             || b->house_size == 2 && game_time_day() % 8
             || b->house_size == 3 && game_time_day() % 4
             || b->house_size == 3 && game_time_day() % 16 && random % 2 // 3x3 consumes 4.5 times a month not 4
