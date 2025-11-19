80edb8148a2df47a37382590e475d368357dc234
miguelinileugim <Miguelinileugim@protonmail.com>
2025-11-19 22:15:26 +0100

    tiny addition

--- src/building/properties.c
+++ src/building/properties.c
@@ -1827,6 +1827,7 @@ static building_properties properties[BUILDING_TYPE_MAX] = {
      [BUILDING_LARARIUM] = {
         .venus_gt_bonus = 1,
         .size = 1,
+        .fire_proof = 1,
         .draw_desirability_range = 1,
         .custom_asset.group = "Health_Culture",
         .custom_asset.id = "Lararium 01",
