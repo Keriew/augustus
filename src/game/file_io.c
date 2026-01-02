0bdaafd6cf07f042ec1cf7f0c936b282185082d4
johannes <johannes@familie-puettmann.de>
2026-01-02 15:41:15 +0100

    migration

--- src/game/file_io.c
+++ src/game/file_io.c
@@ -781,6 +781,9 @@ static void scenario_load_from_state(scenario_state *file, scenario_version_t ve
         production_rates_load(file->production_rates);
     }
     scenario_events_assign_parent_event_ids();
+    if (version <= SCENARIO_TESTING_VERSION_BUMP_2) {
+        scenario_events_migrate_to_buys_sells();
+    }
 
     buffer_skip(file->end_marker, 4);
 }
@@ -973,13 +976,13 @@ static void savegame_load_from_state(savegame_state *state, savegame_version_t v
     if (version <= SAVE_GAME_LAST_NO_FORMULAS_AND_MODEL_DATA) {
         scenario_events_migrate_to_formulas();
         scenario_events_migrate_to_resolved_display_names();
-
-    }
-    if (version <= SAVE_GAME_LAST_NO_FORMULAS_AND_MODEL_DATA) {
         scenario_events_migrate_to_grid_slices();
         scenario_events_min_max_migrate_to_formulas();
     }
     scenario_events_assign_parent_event_ids();
+    if (version <= SAVE_GAME_TESTING_VERSION_BUMP_2) {
+        scenario_events_migrate_to_buys_sells();
+    }
 }
 
 static void savegame_save_to_state(savegame_state *state)
