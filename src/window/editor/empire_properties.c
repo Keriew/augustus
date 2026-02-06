eca0b09067cca1e7fda077fa215b64f3d61bab6c
johannes <johannes@familie-puettmann.de>
2026-02-06 21:19:57 +0100

    prepare buttons

--- src/window/editor/empire_properties.c
+++ src/window/editor/empire_properties.c
@@ -1,5 +1,6 @@
 #include "empire_properties.h"
 
+#include "graphics/button.h"
 #include "graphics/generic_button.h"
 #include "graphics/graphics.h"
 #include "graphics/panel.h"
@@ -8,11 +9,16 @@
 #include "input/input.h"
 #include "input/mouse.h"
 
+static struct {
+    unsigned int focus_button_id;
+} data;
 
+static void button_select_image(const generic_button *button);
 
 static generic_button generic_buttons[] = {
-    {}
+    {16, 16, 200, 30, button_select_image}
 };
+#define NUM_GENERIC_BUTTONS sizeof(generic_buttons) / sizeof(generic_button)
 
 static void init(void)
 {
@@ -24,7 +30,7 @@ static void draw_background(void)
     window_draw_underlying_window();
     graphics_in_dialog();
 
-    outer_panel_draw(64, 32, 32, 18);
+    outer_panel_draw(0, 0, 40, 30);
 
 
     graphics_reset_dialog();
@@ -34,18 +40,31 @@ static void draw_foreground(void)
 {
     graphics_in_dialog();
     
+    for (int i = 0; i < NUM_GENERIC_BUTTONS; i++) {
+        button_border_draw(generic_buttons[i].x, generic_buttons[i].y, generic_buttons[i].width,
+            generic_buttons[i].height, data.focus_button_id == 1);
+    }
+    
     graphics_reset_dialog();
 }
 
 static void handle_input(const mouse *m, const hotkeys *h)
 {
-    //const mouse *m_dialog = mouse_in_dialog(m);
+    const mouse *m_dialog = mouse_in_dialog(m);
+    
+    if (generic_buttons_handle_mouse(m_dialog, 0, 0, generic_buttons, NUM_GENERIC_BUTTONS, &data.focus_button_id)) {
+        return;
+    }
     
     if (input_go_back_requested(m, h)) {
         window_go_back();
     }
 }
 
+static void button_select_image(const generic_button *button)
+{
+    
+}
 
 void window_empire_properties_show(void)
 {
