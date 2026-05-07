bbf578ff852b51d65008440a0df8ac5a0351a2cc
crudelios <crudelios@gmail.com>
2026-05-07 17:16:52 +0100

    Restore CMAKE_PREFIX_PATH

--- .ci_scripts/run_cmake.sh
+++ .ci_scripts/run_cmake.sh
@@ -9,6 +9,7 @@ then
   if [[ "$SDL_MAJOR_VERSION" == "3" ]]
   then
 	CMAKE_SDL_VERSION="-DSDL_VERSION=3"
+	CMAKE_SDL_PREFIX="-DCMAKE_PREFIX_PATH=$PWD/ext/SDL3/SDL3-$SDL_VERSION;$PWD/ext/SDL3/SDL3_mixer-$SDL_MIXER_VERSION"
   fi
 fi
 
@@ -31,16 +32,16 @@ case "$BUILD_TARGET" in
 	mkdir build && cd build && cmake -DAV1_VIDEO_SUPPORT=ON -DCMAKE_BUILD_TYPE=Release $CMAKE_SDL_VERSION -DCMAKE_INSTALL_PREFIX=/usr ..
 	;;
 "linux")
-	mkdir build && cd build && cmake -DAV1_VIDEO_SUPPORT=ON -DCMAKE_BUILD_TYPE=Release $CMAKE_SDL_VERSION ..
+	mkdir build && cd build && cmake -DAV1_VIDEO_SUPPORT=ON -DCMAKE_BUILD_TYPE=Release $CMAKE_SDL_VERSION "$CMAKE_SDL_PREFIX" ..
 	;;
 "android")
 	mkdir build
 	;;
 "emscripten")
 	export EMSDK=${PWD}/emsdk
-	mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release $CMAKE_SDL_VERSION -DTARGET_PLATFORM=emscripten ..
+	mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release $CMAKE_SDL_VERSION "$CMAKE_SDL_PREFIX" -DTARGET_PLATFORM=emscripten ..
 	;;
 *)
-	mkdir build && cd build && cmake -DAV1_VIDEO_SUPPORT=ON $CMAKE_SDL_VERSION ..
+	mkdir build && cd build && cmake -DAV1_VIDEO_SUPPORT=ON $CMAKE_SDL_VERSION "$CMAKE_SDL_PREFIX" ..
 	;;
 esac
