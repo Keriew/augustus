bc95b8b2f197b979ccd2c61f76cbf6bf510e1d48
johannes <johannes@familie-puettmann.de>
2026-07-29 23:43:37 +0200

    variable names in formulas

--- src/scenario/event/formula.c
+++ src/scenario/event/formula.c
@@ -51,6 +51,17 @@ static double parse_number(const unsigned char **s)
     return val + frac / divisor;
 }
 
+static double parse_variable_name(const unsigned char **s)
+{
+    uint8_t name[CUSTOM_VARIABLE_NAME_LENGTH];
+    for (int i = 0; isalpha(**s); i++) {
+        name[i] = (**s);
+        (*s)++;
+    }
+
+    return scenario_custom_variable_get_id_by_name(name);
+}
+
 static double parse_factor(const unsigned char **s)
 {
     skip_spaces(s);
@@ -62,7 +73,8 @@ static double parse_factor(const unsigned char **s)
         return val;
     } else if (**s == '[') {
         (*s)++;
-        int id = (int) parse_number(s);
+        int is_letter = isalpha(**s);
+        int id = is_letter ? parse_variable_name(s) : (int) parse_number(s);
         if (**s == ']') (*s)++;
         return get_var_value(id);
     } else if (**s == '{') {
@@ -165,14 +177,23 @@ int scenario_event_formula_check(scenario_formula_t *s_formula)
         }
         if (*s == '[') {
             s++; // Move past '['
-            if (!isdigit(*s)) { // Check if there's at least one digit
-                s_formula->is_error = 1;
-                return 0; // Invalid: no number after [
-            }
             int variable_id = 0;
-            while (isdigit(*s)) { // Parse the variable ID
-                variable_id = variable_id * 10 + (*s - '0');
-                s++;
+            if (isalpha(*s)) {
+                uint8_t name[CUSTOM_VARIABLE_NAME_LENGTH];
+                for (int i = 0; isalpha(*s); i++) {
+                    name[i] = (*s);
+                    s++;
+                }
+                variable_id = scenario_custom_variable_get_id_by_name(name);
+            } else {
+                while (isdigit(*s)) { // Parse the variable ID
+                    variable_id = variable_id * 10 + (*s - '0');
+                    s++;
+                }
+            }
+            if (!variable_id) {
+                s_formula->is_error = 1;
+                return 0; // Invalid: no valid variable
             }
             if (*s != ']') { // Check for closing bracket
                 s_formula->is_error = 1;
@@ -187,6 +208,9 @@ int scenario_event_formula_check(scenario_formula_t *s_formula)
         } else if (*s == ']') {
             s_formula->is_error = 1;
             return 0; // Invalid: ] without matching [
+        } else if (isalpha(*s)) {
+            s_formula->is_error = 1;
+            return 0; // Invalid: letter which isn't part of a variable identifier
         } else {
             s++;
         }
