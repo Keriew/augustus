f02218dbc2b22c883530adf9740f54f6d83c5ab1
johannes <johannes@familie-puettmann.de>
2026-08-13 20:32:53 +0200

    try different path

--- .ci_scripts/build_upload.ps1
+++ .ci_scripts/build_upload.ps1
@@ -35,7 +35,7 @@ if ("${env:COMPILER}" -eq "msvc") {
     $suffix = "windows-arm64"
     CopyFile build/augustus.exe .
     CopyFile build/augustus.pdb .
-    CopyFile ext\SDL$SDL_MAJOR_VERSION\SDL${SDL_MAJOR_VERSION}\SDL${SDL_MAJOR_VERSION}.dll .
+    CopyFile SDL${SDL_MAJOR_VERSION}-${env:SDL_VERSION}\build\Release\SDL${SDL_MAJOR_VERSION}.dll .
     CopyFile ext\SDL$SDL_MAJOR_VERSION\SDL${SDL_MAJOR_VERSION}_mixer\SDL${SDL_MAJOR_VERSION}_mixer.dll .
 } elseif ("${env:COMPILER}" -eq "mingw-32") {
     $suffix = "windows"
