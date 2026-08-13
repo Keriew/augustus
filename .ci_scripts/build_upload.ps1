7472b15865edd4db6d0baedc9c8b917729752e61
johannes <johannes@familie-puettmann.de>
2026-08-13 20:40:45 +0200

    do it for mixer as well
    I have tried with AI only thing that actually worked was thinking myself what a shame

--- .ci_scripts/build_upload.ps1
+++ .ci_scripts/build_upload.ps1
@@ -36,7 +36,7 @@ if ("${env:COMPILER}" -eq "msvc") {
     CopyFile build/augustus.exe .
     CopyFile build/augustus.pdb .
     CopyFile SDL${SDL_MAJOR_VERSION}-${env:SDL_VERSION}\build\Release\SDL${SDL_MAJOR_VERSION}.dll .
-    CopyFile ext\SDL$SDL_MAJOR_VERSION\SDL${SDL_MAJOR_VERSION}_mixer\SDL${SDL_MAJOR_VERSION}_mixer.dll .
+    CopyFile SDL${SDL_MAJOR_VERSION}_mixer-${env:SDL_VERSION}\build\Release\SDL${SDL_MAJOR_VERSION}_mixer.dll .
 } elseif ("${env:COMPILER}" -eq "mingw-32") {
     $suffix = "windows"
     build/cv2pdb.exe build/augustus.exe
