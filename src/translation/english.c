ee281ed4cf2c71c2245b9f531f7e98aefd334673
Sephirex <15087297+Sephirex95@users.noreply.github.com>
2026-08-22 13:47:31 +0100

    Update src/translation/english.c
    Slightly more intuitive phrasing imo

--- src/translation/english.c
+++ src/translation/english.c
@@ -2231,7 +2231,7 @@ static translation_string all_strings[] = {
     {TR_CITY_MESSAGE_TEXT_TRIUMPHAL_ARCH_COMPLETE, "The triumphal arch now stands complete, its towering stonework and finely carved facade honoring the courage of our soldiers and the victories they have secured for the city. May it stand for generations as a symbol of strength, sacrifice and civic pride."},
     {TR_BUILDING_TRIUMPHAL_ARCH_SUPPLIED_BY_ROME, "(Supplied by Rome)"},
     {TR_WARNING_ENEMIES_PREVENT_BRIDGE_DESTRUCTION, "You can not destroy bridges while an invasion is ongoing!"},
-    {TR_CONFIG_GP_CH_ENEMIES_RETREAT_FAST, "Retreating enemies are twice as fast"},
+    {TR_CONFIG_GP_CH_ENEMIES_RETREAT_FAST, "Enemies retreat twice as fast"},
     {TR_CONFIG_CATEGORY_MANAGEMENT_MILITARY, "Military"},
     {TR_CHEAT_CHANGED_MONUMENT_RESOURCES, "Changed needed resources for monument type"},
     {TR_ACTION_TYPE_CHANGE_MONUMENT_RESOURCES, "Change monument resources needed"},
